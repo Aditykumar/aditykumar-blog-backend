a4b69818774b500a165ffe9b1f2c414c8a7ff066
Aditykumar <adityakushwah29@gmail.com>
2022-02-18 13:55:27 +0530

    f

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use("/api/v1", router);
 
-app.listen(7000, ()=>{
+app.listen(process.env.PORT || 7000, ()=>{
     console.log("Application has started successfully");
 })
 
