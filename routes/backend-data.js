efe24da13e30ebc69ab6c410973f7b91bede9f2f
Aditykumar <adityakushwah29@gmail.com>
2022-02-21 18:30:42 +0530

    f

--- routes/backend-data.js
+++ routes/backend-data.js
@@ -2,8 +2,8 @@ const express = require("express");
 const blogController = require("../controllers/backend-data")
 const blogRouter = express.Router();
 
-// blogRouter.route("/details")
-//     .get(blogController.blogData)
+blogRouter.route("/details")
+    .get(blogController.blogData)
     blogRouter.route("/details/:Category")
     .get(blogController.blogData)
     
